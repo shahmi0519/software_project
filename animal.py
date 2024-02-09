fdbe2b23be0bde12178ee8b4cb1ad80a0988b987
ahamed <shahmiahamed0519@gmail.com>
2024-02-09 14:28:53 +0530

    dog function adde

--- animal.py
+++ animal.py
@@ -1,14 +1,14 @@
 import sys
 
-def cat():
-    print("Meow")
+def dog():
+    print("Baw")
 
 def default():
     print('Hello')
 
 def main():
-    if sys.argv[1] == 'cat':
-        cat()
+    if sys.argv[1] == 'dog':
+        dog()
     else:
         default()
 
