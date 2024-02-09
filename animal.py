3b7a301bb9f8075d791b5bef99625a378fcd217d
ahamed <shahmiahamed0519@gmail.com>
2024-02-09 14:06:49 +0530

    cat function added

---
+++ animal.py
@@ -0,0 +1,20 @@
+import sys
+
+def cat():
+    print("Meow")
+
+def default():
+    print('Hello')
+
+def main():
+    if sys.argv[1] == 'cat':
+        cat()
+    else:
+        default()
+
+
+if __name__  == '__main__':
+    main()
+
+
+
